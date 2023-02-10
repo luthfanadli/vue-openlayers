5345b322bf8f7d7209a8bd689f4c28a2c4929d82
luthfanadli <luthfan30@gmail.com>
2023-02-10 14:35:02 +0700

    initiate vue with openlayers map

---
+++ map-vue-openlayers/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <MapOl></MapOl>
+</template>
+ 
+<script>
+import MapOl from './components/MapOl.vue'
+ 
+export default {
+  name: 'App',
+  components: {
+    MapOl
+  }
+}
+</script>
+ 
+<style>
+@import url('../node_modules/ol/src/ol.css');
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ 
+html, body, #map, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
